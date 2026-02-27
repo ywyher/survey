d860bea5a99cdb229ebbd751711fcbc61a38c20c
ywyh <yassienwyh0@gmail.com>
2026-02-27 09:52:52 +0200

    feat: loading for delete button

--- src/components/delete.tsx
+++ src/components/delete.tsx
@@ -12,28 +12,37 @@ import {
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Button } from "@/components/ui/button";
+import LoadingButton from "@/components/ui/loading-button";
 import { deleteResponse } from "@/lib/actions";
 import { Response } from "@/lib/db/types";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { Trash2 } from "lucide-react";
+import { useState } from "react";
 import { toast } from "sonner";
 
 export default function Delete({ response }: { response: Response }) {
+  const [isLoading, setIsLoading] = useState(false)
   const queryClient = useQueryClient();
 
   const { mutate: remove, isPending: isDeleting } = useMutation({
     mutationFn: deleteResponse,
     onSuccess: (result, id) => {
+      setIsLoading(true)
       if (result.error) {
         toast.error(result.error);
+        setIsLoading(false)
         return;
       }
       toast.success("Response deleted.");
       queryClient.setQueryData(["responses"], (old: Response[]) =>
         old?.filter((s) => s.id !== id),
       );
+      setIsLoading(false)
     },
-    onError: () => toast.error("Failed to delete."),
+    onError: () => {
+      toast.error("Failed to delete.")
+      setIsLoading(false)
+    },
   });
 
   return (
@@ -64,6 +73,14 @@ export default function Delete({ response }: { response: Response }) {
           <AlertDialogAction
             onClick={() => remove(response.id)}
             className="bg-destructive text-destructive-foreground hover:bg-destructive/90"
+            render={
+              <LoadingButton
+                variant="destructive"
+                isLoading={isLoading}
+              >
+                Delete
+              </LoadingButton>
+            }
           >
             Delete
           </AlertDialogAction>
